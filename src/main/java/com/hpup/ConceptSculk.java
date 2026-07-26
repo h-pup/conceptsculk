15a189443a08d3c000f60ddf0e53694af9e69558
h-pup <213760485+h-pup@users.noreply.github.com>
2026-07-26 22:31:58 +1000

    initial commit

---
+++ src/main/java/com/hpup/ConceptSculk.java
@@ -0,0 +1,23 @@
+package com.hpup;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConceptSculk implements ModInitializer {
+	public static final String MOD_ID = "conceptsculk";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info(MOD_ID + " Loaded");
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
---
+++ src/main/java/com/hpup/ConceptSculkDataGenerator.java
@@ -0,0 +1,11 @@
+package com.hpup;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ConceptSculkDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
