5cd90bc23fa7e7077ebea5b519fb35bcf1857f01
h-pup <213760485+h-pup@users.noreply.github.com>
2026-07-27 15:35:18 +1000

    make Ancient Vases face the direction you placed them

--- src/main/java/com/hpup/block/AncientVaseBlock.java
+++ src/main/java/com/hpup/block/AncientVaseBlock.java
@@ -1,19 +1,51 @@
 package com.hpup.block;
 
+import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class AncientVaseBlock extends Block {
+public class AncientVaseBlock extends HorizontalDirectionalBlock {
+    public static final MapCodec<AncientVaseBlock> CODEC = Block.simpleCodec(AncientVaseBlock::new);
+
     public AncientVaseBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(final StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    protected MapCodec<? extends AncientVaseBlock> codec() {
+        return CODEC;
     }
 
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return Block.box(3, 0, 3, 13, 10, 13);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(final BlockPlaceContext context) {
+        FluidState replacedFluidState = context.getLevel().getFluidState(context.getClickedPos());
+        return super.getStateForPlacement(context).setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public boolean isPathfindable(final BlockState state, final PathComputationType type) {
+        return false;
     }
 }
